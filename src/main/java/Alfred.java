6bf8754be18ce4649c529ce016e4307d31112c93
Hack-Zac <isaacjasonabraham@gmail.com>
2026-01-22 23:02:07 +0800

    Added echo function

--- src/main/java/Alfred.java
+++ src/main/java/Alfred.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
 public class Alfred {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Alfred");
         System.out.println("What can I do for you?");
         System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+
+
+            } else {
+                System.out.println(input);
+
+
+            }
+
+            scanner.close();
+
+        }
 
     }
 }
