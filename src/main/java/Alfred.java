3e408e232dcda7749a1a180c89e6f46ecd933948
Hack-Zac <isaacjasonabraham@gmail.com>
2026-01-23 10:44:32 +0800

    Added level 3 functionality

--- src/main/java/Alfred.java
+++ src/main/java/Alfred.java
@@ -3,24 +3,40 @@ public class Alfred {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] tasks = new String[100];
+        boolean[] completed = new boolean[100];
         int count = 0;
 
-        System.out.println("Hello! I'm Alfred");
-        System.out.println("What can I do for you?");
+        System.out.println("Hello! I'm Alfred\n");
+        System.out.println("What can I do for you?\n");
 
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("Bye. Hope to see you again soon!\n");
                 break;
 
 
             } else if (input.equals("list")) {
                 for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    String status = completed[i] ? "X" : " ";
+                    System.out.println((i + 1) + ". [" + status + "] " + tasks[i] +"\n");
+
                 }
+            } else if (input.startsWith("mark ")){
+                int index = Integer.parseInt(input.substring(5))-1;
+                completed[index] = true;
+                System.out.println("Nice! I've marked this task as done:\n");
+
+            } else if (input.startsWith("unmark ")){
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                completed[index] = false;
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [ ] " + tasks[index]);
+
+
             } else {
                 tasks[count] = input;
+                completed[count] = false;
                 count++;
                 System.out.println("added: " + input);
             }
